dcc3a62ceea73a37607da7393e29c0178c73fc25
nirkaufman <nirkaufman@gmail.com>
2017-10-19 15:27:20 +0300

    simple component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,35 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ title }}</h1>
+    <h1>List</h1>
+
+    <input type="text"
+           #item
+           placeholder="add item..">
+
+    <button (click)="addItem(item.value)">add</button>
+
+    <ul>
+      <li *ngFor="let item of items">{{ item }}</li>
+    </ul>
   `
 })
+
 export class AppComponent {
-  public title: string;
+
+  public items: string[];
 
   constructor() {
-    this.title = 'Hello Angular';
+    this.items = [];
   }
+
+  public addItem(item: string): void {
+    this.items.push(item);
+  }
+
+  public removeItem(item: string): void {
+    const index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+
 }
