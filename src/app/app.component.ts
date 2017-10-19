09d90e66707c9e6eccae4457f2e9c6f919390108
nirkaufman <nirkaufman@gmail.com>
2017-10-19 13:53:20 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  public title: string;
+
+  constructor() {
+    this.title = 'Hello Angular';
+  }
 }
