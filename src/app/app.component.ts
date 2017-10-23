cbfb2603cb943b6c99c9763a5b7e361e72de8c1f
nirkaufman <nirkaufman@gmail.com>
2017-10-23 09:32:39 +0300

    the todolist init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,10 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Child Components</h1>
-
-    <app-child title="title"
-               text="click me"
-               (childClick)="test($event)"></app-child>
+    <h1>The Todolist</h1>
   `
 })
 
 export class AppComponent {
-
-  test(event) {
-    console.log(event);
-  }
 
 }
