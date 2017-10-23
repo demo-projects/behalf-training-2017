84b86c6a50bd91cbefbb5913a42a7f01ba89840f
nirkaufman <nirkaufman@gmail.com>
2017-10-23 16:10:38 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {HistoryModule} from './history/history.module';
+import {LoginModule} from './login/login.module';
 import {TodolistModule} from './todolist/todolist.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports     : [BrowserModule, TodolistModule],
+  imports     : [
+    BrowserModule,
+    LoginModule,
+    TodolistModule,
+    HistoryModule
+  ],
   bootstrap   : [AppComponent]
 })
 
