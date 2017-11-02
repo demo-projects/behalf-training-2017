b3a1ab2bf8baddf8701371157cf29dabd8d611e6
nirkaufman <nirkaufman@gmail.com>
2017-11-02 10:48:56 +0200

    redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,16 @@
 import {Component} from '@angular/core';
-import {TodolistService} from './todolist/todolist.service';
 
 @Component({
-  selector: 'app-root',
-  providers: [TodolistService],
-  template: `
+  selector : 'app-root',
+  template : `
     <a routerLink="login">login</a>
     <a routerLink="list">list</a>
     <a routerLink="log">history</a>
 
-    <pre>{{ list.items | json }}</pre>
-
-    <app-todolist></app-todolist>
-    <!--<router-outlet></router-outlet>-->
+    <!--routing placeholder-->
+    <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent {
-
-  constructor(public list: TodolistService) {
-
-  }
-
 }
